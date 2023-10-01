b810ef85ebd8b19000865d6990732ed8069ee02f
impactaly <impactaky@users.noreply.github.com>
2023-10-01 13:48:41 +0900

    Use patched grpc

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { getClient } from "https://deno.land/x/grpc_basic@0.4.7/client.ts";
-export type { GrpcClient } from "https://deno.land/x/grpc_basic@0.4.7/client.ts";
+export { getClient } from "https://raw.githubusercontent.com/impactaky/deno-grpc/0.4.7.1/client.ts";
+export type { GrpcClient } from "https://raw.githubusercontent.com/impactaky/deno-grpc/0.4.7.1/client.ts";
 export * from "./protos/kachaka-api.d.ts";
