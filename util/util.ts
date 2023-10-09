f2d85470dd3523bbc4b3e0ef2cdada07118d3a61
impactaky <37619203+impactaky@users.noreply.github.com>
2023-10-09 22:02:47 +0900

    Add support pngMap to mock server (#9)
    Co-authored-by: impactaly <impactaky@users.noreply.github.com>

--- util/util.ts
+++ util/util.ts
@@ -1,3 +1,17 @@
+import { decodeBase64 } from "https://deno.land/std@0.203.0/encoding/base64.ts";
+
 export async function fetchText(url: URL) {
   return (await fetch(url)).text();
+}
+
+function reviver<T>(key: string, value: T): T | Uint8Array {
+  if (key === "data" && typeof value === "string") {
+    return decodeBase64(value);
+  }
+  return value;
+}
+
+export async function createFromJson<T>(url: URL): Promise<T> {
+  const content = await fetchText(url);
+  return JSON.parse(content, reviver);
 }
