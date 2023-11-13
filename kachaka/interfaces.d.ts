7d70e22783f81e3a953c49fe69d3ae591b98fc25
impactaky <37619203+impactaky@users.noreply.github.com>
2023-11-13 19:12:48 +0900

    Support callback (#11)
    * Refactroing
    
    * Add callback feature
    
    * Refactoring
    
    ---------
    
    Co-authored-by: impactaly <impactaky@users.noreply.github.com>

---
+++ kachaka/interfaces.d.ts
@@ -0,0 +1,7 @@
+import { pb } from "../deps.ts";
+
+export interface WithMetadata {
+  metadata?: pb.Metadata;
+}
+
+export type WithoutMetadata<T extends WithMetadata> = Omit<T, "metadata">;
---
+++ kachaka/value_handler.ts
@@ -0,0 +1,59 @@
+import { pb } from "../deps.ts";
+import { WithMetadata } from "./interfaces.d.ts";
+
+export class ValueHandler<T extends object & WithMetadata, U, V, W> {
+  #getFunction;
+  #setFunction;
+  #pickFunction;
+  #callbackFuncitons: Array<(result: U) => void> = [];
+  constructor(
+    getFunction: (request: pb.GetRequest) => Promise<T>,
+    pickFunction: (response: T) => U,
+    setFunction?: (request: V) => Promise<W>,
+  ) {
+    this.#getFunction = getFunction;
+    this.#pickFunction = pickFunction;
+    this.#setFunction = setFunction;
+  }
+  async get(): Promise<U>;
+  async get(cursor: number): Promise<T>;
+  async get(cursor?: number) {
+    if (cursor === undefined) {
+      const response = await this.#getFunction({ metadata: { cursor: 0 } });
+      return this.#pickFunction(response);
+    }
+    return await this.#getFunction({ metadata: { cursor: cursor } });
+  }
+
+  set(request: V) {
+    return this.#setFunction!(request);
+  }
+
+  async callbackLoop() {
+    let cursor = (await this.get(0)).metadata!.cursor;
+    while (this.#callbackFuncitons.length > 0) {
+      const response = await this.get(cursor!);
+      cursor = response.metadata!.cursor!;
+      this.#callbackFuncitons.forEach((cb) => cb(this.#pickFunction(response)));
+    }
+  }
+  registerCallback(callback: (result: U) => void) {
+    const isCallbackRunning = this.#callbackFuncitons.length > 0;
+    this.#callbackFuncitons.push(callback);
+    if (!isCallbackRunning) {
+      this.callbackLoop();
+    }
+  }
+  unregisterCallback(callback: (result: U) => void) {
+    this.#callbackFuncitons = this.#callbackFuncitons.filter(
+      (cb) => cb !== callback,
+    );
+  }
+  registerCallbackOnce(callback: (result: U) => void) {
+    const once = (result: U) => {
+      this.unregisterCallback(once);
+      callback(result);
+    };
+    this.registerCallback(once);
+  }
+}
